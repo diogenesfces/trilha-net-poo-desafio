1478ee1d1e73dec315fe8717f42c85e97ed4192e
diogenesfces <112892924+diogenesfces@users.noreply.github.com>
2022-11-08 15:40:46 -0300

    Update Program.cs
    Atualização dos testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "01", modelo: "N3420", imei: "983798713", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Microsoft Teams");
+Console.WriteLine("...................................");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "02", modelo: "IPHONE 11", imei: "4535462246", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+Console.WriteLine("...................................");
