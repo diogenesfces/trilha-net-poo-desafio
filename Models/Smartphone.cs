e84d3656361d19e2be74ea0405c200c91b863e74
diogenes.silva <diogenesfces@gmail.com>
2022-11-08 15:36:32 -0300

    Desafio POO finalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,26 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"O número de celular é: {Numero}, do {Modelo}, com Imei: {Imei}, e tem {Memoria} de memória.");
+        }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
